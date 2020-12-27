30cb9c7124f70deedd17e2f31d856ea551a44a51
kreid333 <kai.j.reid@gmail.com>
2020-12-27 14:14:07 -0500

    installed packages and setup basic server

---
+++ server.js
@@ -0,0 +1,46 @@
+// IMPORTING PACKAGES
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// DEFINING PORT
+const PORT = process.env.PORT || 3001;
+
+// CREATING INSTANCE OF EXPRESS
+const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// CONNECTING TO MONGODB DATABASE
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/thesocialsite", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// TESTING CONNECTION TO DATABASE
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+// TESTING GET REQUEST ON SERVER
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+// LISTENING ON LOCALHOST
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
