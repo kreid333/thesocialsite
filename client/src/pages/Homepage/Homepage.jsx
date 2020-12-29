96f293d5924c38ba288925f43254136ee826f92d
kreid333 <kai.j.reid@gmail.com>
2020-12-29 13:30:45 -0500

    edited styling for signup/login page

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -10,6 +10,7 @@ const Homepage = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-6" id="left">
+          <h1 id="logo">TheSocialSite</h1>
           <div className="ml-3" id="left-content">
             <div className="row mb-5">
               <div className="col-xs-3">
