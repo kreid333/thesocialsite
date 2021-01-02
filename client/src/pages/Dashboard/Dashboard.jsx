5330735babc3fba335ee6491b1d1951db994b0df
kreid333 <kai.j.reid@gmail.com>
2021-01-02 15:45:54 -0500

    added navigation buttons (not linked as of yet)

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCog,
+  faCommentAlt,
+  faSearch,
+  faUser,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div id="top-card" className="text-center">
-        <FontAwesomeIcon icon={faUser} size="2x" className="mt-2"/>
-        <p>Welcome, username!</p>
+    <>
+      <div id="nav-bar">
+        <div id="top-card" className="text-center">
+          <FontAwesomeIcon icon={faUser} size="2x" className="mt-2" />
+          <p>Welcome, username!</p>
+        </div>
+        <div className="text-center">
+          <span>
+            <FontAwesomeIcon icon={faSearch} size="4x" className="fa-icon" />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              icon={faUserCircle}
+              size="4x"
+              className="fa-icon"
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon
+              icon={faCommentAlt}
+              size="4x"
+              className="fa-icon"
+            />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faCog} size="4x" className="fa-icon" />
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
