39dec6a65768d8cf4262b4167ea2f539024f1422
kreid333 <kai.j.reid@gmail.com>
2021-01-02 14:12:10 -0500

    added user card at the top of the page

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div id="top-card" className="text-center">
+        <FontAwesomeIcon icon={faUser} size="2x" className="mt-2"/>
+        <p>Welcome, username!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
